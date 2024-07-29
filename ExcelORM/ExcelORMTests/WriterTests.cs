8b758fab3865a09efdef6f2ed916e5bbad568668
Piotr Czajkowski <pczajkowski@gmail.com>
2024-07-29 18:27:21 +0200

    Test everything

--- ExcelORM/ExcelORMTests/WriterTests.cs
+++ ExcelORM/ExcelORMTests/WriterTests.cs
@@ -90,6 +90,29 @@ public class WriterTests
         File.Delete(testFile);
     }
 
+    private const string ForAppendHeaderFirst = "testFiles/forAppendHeaderFirst.xlsx";
+
+    [Fact]
+    public void WriteWithAppendExistingHeaderFirst()
+    {
+        var testFile = Path.GetRandomFileName();
+        testFile = Path.ChangeExtension(testFile, "xlsx");
+        File.Copy(ForAppendHeaderFirst, testFile);
+
+        var writer = new ExcelWriter(testFile);
+        writer.Write(arrayOfThree, append: true);
+        writer.SaveAs(testFile);
+
+        var reader = new ExcelReader(testFile);
+        var readArray = reader.Read<Test>().ToArray();
+        Assert.Equal(6, readArray.Length);
+
+        for (int i = 0; i < arrayOfThree.Length; i++)
+            Assert.Equal(arrayOfThree[i], readArray[i + 3]);
+
+        File.Delete(testFile);
+    }
+
     [Fact]
     public void WriteDifferentTypes()
     {
