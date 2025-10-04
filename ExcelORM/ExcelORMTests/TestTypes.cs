b6da0a6972ea03436de727f913152d1d65d1b1b0
Piotr Czajkowski <pczajkowski@gmail.com>
2025-10-04 17:33:58 +0200

    Testing more types

--- ExcelORM/ExcelORMTests/TestTypes.cs
+++ ExcelORM/ExcelORMTests/TestTypes.cs
@@ -7,4 +7,9 @@ public record TestTypes
     public TimeSpan? TimeSpan { get; set; }
     public int? Int { get; set; }
     public double? Double { get; set; }
+    public decimal? Decimal { get; set; }
+    public long? Long { get; set; }
+    public short? Short { get; set; }
+    public float? Float { get; set; }
+    public byte? Byte { get; set; }
 }
